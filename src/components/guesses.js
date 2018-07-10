6caaee8226e4756e3570dc37dc580a8cf3e985ee
Rodrigo Santelices <rcsantelices@gmail.com>
2018-07-09 22:08:15 -0400

    what? and input clear left

--- src/components/guesses.js
+++ src/components/guesses.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
 export default function Guesses(props){
+    const guessList = props.guesses.map((guess, index) => (
+        <p key={index}>
+        {guess}
+        </p>
+    ))
     return (
         <div className= "guess-record">
-            {props.guesses}
+            {guessList}
         </div>
     )
 }
